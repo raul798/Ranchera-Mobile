0e58374601d8bd299e5883033593ba90e5332fc1
Kelvin Garcia <xaviergarcia19@gmail.com>
2019-02-19 17:49:55 -0400

    First Commit

---
+++ app/src/main/java/comx/example/xavie/loginranchera/MainActivity.java
@@ -0,0 +1,33 @@
+package comx.example.xavie.loginranchera;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void loginMe(View view) {
+        Intent loginIntent = new Intent(this, MenuActivity.class);
+        //login logic
+        startActivity(loginIntent);
+    }
+
+    public void forgotMe(View view) {
+        Intent forgotIntent = new Intent(this, ForgotActivity.class);
+        //login logic
+        startActivity(forgotIntent);
+    }
+
+    public void helpMe(View view) {
+        Intent helpIntent = new Intent(this, SupportActivity.class);
+        //login logic
+        startActivity(helpIntent);
+    }
+}
---
+++ app/src/main/java/comx/example/xavie/loginranchera/SupportActivity.java
@@ -0,0 +1,17 @@
+package comx.example.xavie.loginranchera;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class SupportActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_support);
+    }
+
+
+}
