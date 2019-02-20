3b9675689293e007b702e6c01baaf9bb83b18c55
Edmundy <edmundy.reyes@gmail.com>
2019-02-20 19:56:31 -0400

    flujo inicial para ver los productos y Consulta de Clientes

---
+++ app/src/main/java/ado/edu/pucmm/rancherasystem/ConsultarCliente.java
@@ -0,0 +1,21 @@
+package ado.edu.pucmm.rancherasystem;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class ConsultarCliente extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_consultar__clientes);
+    }
+    public void ToastTBD(View view){
+        // Toast myToast = Toast.makeText(this, message, duration);
+        Toast myToast = Toast.makeText(this, "TO BE DESIGNED",
+                Toast.LENGTH_SHORT);
+        myToast.show();
+    }
+}
