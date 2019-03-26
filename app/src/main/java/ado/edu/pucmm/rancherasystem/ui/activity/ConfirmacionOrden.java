c1ed1e43d71fe263bad2fd6871cd947562178aa6
unknown <raul.arturo798@gmail.com>
2019-03-26 18:55:58 -0400

    fixed action bar in ConfirmacionOrden

--- app/src/main/java/ado/edu/pucmm/rancherasystem/ui/activity/ConfirmacionOrden.java
+++ app/src/main/java/ado/edu/pucmm/rancherasystem/ui/activity/ConfirmacionOrden.java
@@ -23,8 +23,7 @@ import java.io.ByteArrayOutputStream;
 import ado.edu.pucmm.rancherasystem.R;
 import ado.edu.pucmm.rancherasystem.db.RanchDatabaseRepo;
 
-public class ConfirmacionOrden extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
+public class ConfirmacionOrden extends AppCompatActivity {
 
     private Button finishButton;
     private ImageView finishCircle;
@@ -42,17 +41,6 @@ public class ConfirmacionOrden extends AppCompatActivity
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_confirmacion_orden);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawer.addDrawerListener(toggle);
-        toggle.syncState();
-
-        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
-        navigationView.setNavigationItemSelectedListener(this);
 
         //set button unabled
         finishButton = (Button) findViewById(R.id.finish_button);
@@ -99,62 +87,6 @@ public class ConfirmacionOrden extends AppCompatActivity
         if (extras != null) {
             bill_id = extras.getInt("billId");
         }
-    }
-
-    @Override
-    public void onBackPressed() {
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        if (drawer.isDrawerOpen(GravityCompat.START)) {
-            drawer.closeDrawer(GravityCompat.START);
-        } else {
-            super.onBackPressed();
-        }
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.confirmacion_orden, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.helpButton) {
-            Intent testIntent = new Intent(this, MainActivity.class);
-            startActivity(testIntent);
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
-    @SuppressWarnings("StatementWithEmptyBody")
-    @Override
-    public boolean onNavigationItemSelected(MenuItem item) {
-        // Handle navigation view item clicks here.
-        int id = item.getItemId();
-
-        if (id == R.id.nav_resumen) {
-            Intent testIntent = new Intent(this, MainActivity.class);
-            startActivity(testIntent);
-
-        } else if (id == R.id.nav_productos) {
-
-        } else if (id == R.id.nav_consultar_cliente) {
-
-        } else if (id == R.id.nav_ordenes) {
-            //do nothing
-        }
-
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        drawer.closeDrawer(GravityCompat.START);
-        return true;
     }
 
     private void setText(int resourceId, String text){
