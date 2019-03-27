b40867560b5de170defe8b102ee62d3953fd665d
unknown <raul.arturo798@gmail.com>
2019-03-27 04:07:30 -0400

    added bottom navigator

--- app/src/main/java/ado/edu/pucmm/rancherasystem/ui/activity/MenuActivity.java
+++ app/src/main/java/ado/edu/pucmm/rancherasystem/ui/activity/MenuActivity.java
@@ -2,6 +2,8 @@ package ado.edu.pucmm.rancherasystem.ui.activity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.view.View;
@@ -13,11 +15,14 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 import ado.edu.pucmm.rancherasystem.R;
 
 public class MenuActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+
+    Intent intent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,7 +30,6 @@ public class MenuActivity extends AppCompatActivity
         setContentView(R.layout.activity_menu);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -35,6 +39,34 @@ public class MenuActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setOnNavigationItemReselectedListener(new BottomNavigationView.OnNavigationItemReselectedListener() {
+            @Override
+            public void onNavigationItemReselected(@NonNull MenuItem menuItem) {
+                switch (menuItem.getItemId())
+                {
+                    case R.id.products:
+                        Toast.makeText(MenuActivity.this, "product", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.clients:
+                        Toast.makeText(MenuActivity.this, "clients", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.inventory:
+                        Toast.makeText(MenuActivity.this, "inventory", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.order:
+                        intent = new Intent(MenuActivity.this, SelectClientActivity.class);
+                        startActivity(intent);
+                        break;
+                    case R.id.bill:
+                        intent = new Intent(MenuActivity.this, PagarFacturaActivity.class);
+                        startActivity(intent);
+                        Toast.makeText(MenuActivity.this, "bill", Toast.LENGTH_SHORT).show();
+                        break;
+                }
+            }
+        });
     }
 
     @Override
