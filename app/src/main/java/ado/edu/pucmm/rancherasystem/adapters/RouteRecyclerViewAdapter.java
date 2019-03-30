722012fe757fb28d3eab7ffc1f49941e52d2ff3f
unknown <raul.arturo798@gmail.com>
2019-03-29 20:58:05 -0400

    added toMap button

--- app/src/main/java/ado/edu/pucmm/rancherasystem/adapters/RouteRecyclerViewAdapter.java
+++ app/src/main/java/ado/edu/pucmm/rancherasystem/adapters/RouteRecyclerViewAdapter.java
@@ -1,12 +1,15 @@
 package ado.edu.pucmm.rancherasystem.adapters;
 
 import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -16,6 +19,9 @@ import java.util.List;
 import ado.edu.pucmm.rancherasystem.R;
 import ado.edu.pucmm.rancherasystem.entity.Client;
 import ado.edu.pucmm.rancherasystem.entity.Route;
+import ado.edu.pucmm.rancherasystem.ui.activity.MenuActivity;
+import ado.edu.pucmm.rancherasystem.ui.activity.SelectClientActivity;
+import ado.edu.pucmm.rancherasystem.ui.activity.SelectProductsActivity;
 
 public class RouteRecyclerViewAdapter extends RecyclerView.Adapter<RouteRecyclerViewAdapter.RouteViewHolder> {
 
@@ -23,11 +29,21 @@ public class RouteRecyclerViewAdapter extends RecyclerView.Adapter<RouteRecycler
 
         private final TextView clientNameView;
         private final TextView addressView;
+        private final ImageButton toMap;
+        private final AdapterListener onClickListener;
 
         private RouteViewHolder(View itemView) {
             super(itemView);
             clientNameView = itemView.findViewById(R.id.text_view_name);
             addressView = itemView.findViewById(R.id.address_clientes_text);
+            toMap = itemView.findViewById(R.id.btn_toMap);
+            onClickListener = new AdapterListener();
+            toMap.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    onClickListener.onClick(v, getAdapterPosition());
+                }
+            });
         }
     }
 
@@ -62,5 +78,20 @@ public class RouteRecyclerViewAdapter extends RecyclerView.Adapter<RouteRecycler
     public void setClients(List<Client> clients){
         this.clients = clients;
         notifyDataSetChanged();
+    }
+
+    public class AdapterListener {
+
+        void onClick(View view, int position){
+            /*
+           Context context = view.getContext();
+            Intent intent = new Intent(context, SelectClientActivity.class);
+            context.startActivity(intent);
+            */
+            Context context = view.getContext();
+            Intent intent = new Intent(android.content.Intent.ACTION_VIEW,
+                    Uri.parse("google.navigation:q=Calle Sanabacoa,+Santo Domingo"));
+            context.startActivity(intent);
+        }
     }
 }
